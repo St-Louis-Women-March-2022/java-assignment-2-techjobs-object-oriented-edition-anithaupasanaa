5ced22318ed7aeb4b169d11da2b429a84ab2ce97
anithaupasanaa <anithaupasanaa@gmail.com>
2022-09-28 19:16:51 -0500

    final commit

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs.oo;
 
+import com.sun.jdi.Value;
+
 import java.util.Objects;
 
 public class CoreCompetency {
@@ -40,5 +42,18 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+    public String getValue(){
+        return value;
+    }
+    public void setValue(String value) {
+        this.value=value;
 
-}
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    }
+
+
